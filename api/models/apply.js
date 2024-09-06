7c19532eee2e3ecd8cd2478978ca925484c0df9a
Renan Johny Ortega <renanortega82@gmail.com>
2024-09-06 00:03:44 -0300

    feat: add discord.

--- api/models/apply.js
+++ api/models/apply.js
@@ -3,6 +3,7 @@ const mongoose = require('../config/db');
 const applySchema = new mongoose.Schema({
     name: String,
     nick: String,
+    discord: String,
     position: String,
     elo: String,
     opgg: String,
